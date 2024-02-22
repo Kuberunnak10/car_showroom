137b34859d73488805dfa6152486cd4cb3c4dcb4
Sergey <kuberunnak@ya.ru>
2024-02-22 14:08:44 +0300

    add all imege for marks and get count models

--- app_autosalon/urls.py
+++ app_autosalon/urls.py
@@ -1,9 +1,9 @@
 from django.urls import  path
 
-from app_autosalon.views import index, get_mark, get_auto
+from app_autosalon.views import get_all_marks, get_mark, get_auto
 
 urlpatterns = [
-    path('', index, name='main_page'),
+    path('', get_all_marks, name='main_page'),
     path('cars/<str:mark>', get_mark, name='mark_name'),
     path('cars/sale/<int:id>', get_auto, name='get_auto')
 ]
--- app_autosalon/views.py
+++ app_autosalon/views.py
@@ -4,7 +4,7 @@ from app_autosalon.models import Mark, Auto
 
 
 # Create your views here.
-def index(request):
+def get_all_marks(request):
     mark = cache.get('cache_marks')
     if not mark:
         mark = Mark.objects.all()
@@ -15,11 +15,11 @@ def index(request):
 def get_mark(request, mark):
     mark_name = get_object_or_404(Mark, name=mark)
     autos = Auto.objects.filter(mark=mark_name)
-    return render(request, 'app_autosalon/specific_mark.html', {'mark_name': mark_name, 'autos': autos})
+    count = autos.count()
+    return render(request, 'app_autosalon/specific_mark.html', {'mark_name': mark_name, 'autos': autos, 'count': count})
 
 
 def get_auto(request, id):
     auto_name = get_object_or_404(Auto, id=id)
-
     return render(request, 'app_autosalon/auto.html', {'auto': auto_name})
 
